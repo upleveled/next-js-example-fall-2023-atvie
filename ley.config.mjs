d2070d5fcf4b64eda1c9d2dbcf4024fdb714ff6d
Lukas Prochazka <prochazka.lu@gmail.com>
2023-10-09 14:52:47 +0200

    Add ley.config.mjs SSL options

--- ley.config.mjs
+++ ley.config.mjs
@@ -1,5 +1,9 @@
 import { setEnvironmentVariables } from './util/config.mjs';
 
 setEnvironmentVariables();
-const options = {};
+
+const options = {
+  ssl: Boolean(process.env.POSTGRES_URL),
+};
+
 export default options;
