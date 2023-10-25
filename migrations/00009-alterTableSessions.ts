cf4b4e6204dbeace7e05dda4b7ba8bc118894bca
Victor Ejike Nwosu <ejikello123@yahoo.com>
2023-10-25 13:49:28 +0200

    Fix the alter function no null value error

--- migrations/00009-alterTableSessions.ts
+++ migrations/00009-alterTableSessions.ts
@@ -10,7 +10,16 @@ export type Sessions = {
 export async function up(sql: Sql) {
   await sql`
     ALTER TABLE sessions
-      ADD csrf_secret varchar(80)
+    -- The new column is created with a default value of empty strings
+    -- that will bge added to all the records in this table
+      ADD csrf_secret varchar(80) NOT NULL DEFAULT ''
+  `;
+  await sql`
+    ALTER TABLE sessions
+    -- We don't need the DEFAULT keyword anymore
+    -- So we alter the table again and remote it
+    -- This makes the csrf_secret NOT NULL again
+      ALTER COLUMN csrf_secret DROP DEFAULT
   `;
 }
 
