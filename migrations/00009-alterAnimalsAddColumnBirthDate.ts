b44cda4d7b824d5a82952b94422105cc4bf6a383
Lukas Prochazka <prochazka.lu@gmail.com>
2023-11-02 10:56:35 +0100

    Change default birthDate

--- migrations/00009-alterAnimalsAddColumnBirthDate.ts
+++ migrations/00009-alterAnimalsAddColumnBirthDate.ts
@@ -3,7 +3,7 @@ import { Sql } from 'postgres';
 export async function up(sql: Sql) {
   await sql`
       ALTER TABLE animals
-        ADD COLUMN birth_date date NOT NULL DEFAULT '2023-10-25';
+        ADD COLUMN birth_date date NOT NULL DEFAULT '2020-01-01';
   `;
 }
 
