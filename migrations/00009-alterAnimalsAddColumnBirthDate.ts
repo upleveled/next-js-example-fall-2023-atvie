038d8c1d883fb3905244a4a7dffe5a337bbc08eb
Lukas Prochazka <prochazka.lu@gmail.com>
2023-11-03 17:35:20 +0100

    Change birth_date indentation

--- migrations/00009-alterAnimalsAddColumnBirthDate.ts
+++ migrations/00009-alterAnimalsAddColumnBirthDate.ts
@@ -2,14 +2,14 @@ import { Sql } from 'postgres';
 
 export async function up(sql: Sql) {
   await sql`
-      ALTER TABLE animals
-        ADD COLUMN birth_date date NOT NULL DEFAULT '2020-01-01';
+    ALTER TABLE animals
+    ADD COLUMN birth_date DATE NOT NULL DEFAULT '2020-01-01';
   `;
 }
 
 export async function down(sql: Sql) {
   await sql`
-      ALTER TABLE animals
-        DROP COLUMN birth_date;
-    `;
+    ALTER TABLE animals
+    DROP COLUMN birth_date;
+  `;
 }
