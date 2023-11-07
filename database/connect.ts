a452f9d1ecb9fd55aa49ab45101c23102a4b8a2e
Victor Ejike Nwosu <74430629+Eprince-hub@users.noreply.github.com>
2023-11-07 11:47:35 +0100

    Use unstable_noStore for cache invalidation (#10)
    Co-authored-by: Karl Horky <karl.horky@gmail.com>

--- database/connect.ts
+++ database/connect.ts
@@ -1,5 +1,5 @@
 import 'server-only';
-import { headers } from 'next/headers';
+import { unstable_noStore as noStore } from 'next/cache';
 import postgres, { Sql } from 'postgres';
 import { setEnvironmentVariables } from '../util/config.mjs';
 
@@ -22,23 +22,18 @@ function connectOneTimeToDatabase() {
     });
   }
 
-  // Workaround to force Next.js Dynamic Rendering:
+  // Use Next.js Dynamic Rendering in all database queries:
   //
-  // Wrap sql`` tagged template function to call `headers()` from
-  // next/headers before each database query. `headers()` is a
+  // Wrap sql`` tagged template function to call `noStore()` from
+  // next/cache before each database query. `noStore()` is a
   // Next.js Dynamic Function, which causes the page to use
-  // Dynamic Rendering.
+  // Dynamic Rendering
   //
   // https://nextjs.org/docs/app/building-your-application/rendering/static-and-dynamic-rendering
-  //
-  // Ideally there would something built into Next.js for this,
-  // which has been requested here:
-  //
-  // https://github.com/vercel/next.js/discussions/50695
   return ((
     ...sqlParameters: Parameters<typeof globalThis.postgresSqlClient>
   ) => {
-    headers();
+    noStore();
     return globalThis.postgresSqlClient(...sqlParameters);
   }) as typeof globalThis.postgresSqlClient;
 }
