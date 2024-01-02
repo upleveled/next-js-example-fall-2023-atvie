7bd97a7ca13492662f9a07a83458c7f9c621c082
Lukas Prochazka <prochazka.lu@gmail.com>
2024-01-02 16:16:13 +0100

    Update comment

--- util/dates.ts
+++ util/dates.ts
@@ -22,7 +22,7 @@ export function getDaysUntilNextBirthday(currentDate: Date, birthDate: Date) {
     throw new Error('Birth date must be before current date!');
   }
 
-  // Create new date objects to avoid mutating the original birth date
+  // Create new date objects to avoid mutating the current date and original birth date
   const startOfCurrentDate = new Date(currentDate);
   const nextBirthDate = new Date(birthDate);
 
