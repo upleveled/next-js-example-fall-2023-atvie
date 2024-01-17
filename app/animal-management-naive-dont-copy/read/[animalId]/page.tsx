047e37723b194ef30b60844b10cfe25efb6fbf85
Lukas Prochazka <prochazka.lu@gmail.com>
2024-01-17 22:06:36 +0100

    Display birth date on naive management pages (#19)

--- app/animal-management-naive-dont-copy/read/[animalId]/page.tsx
+++ app/animal-management-naive-dont-copy/read/[animalId]/page.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image';
 import { notFound } from 'next/navigation';
 import { getAnimalById } from '../../../../database/animals';
+import { formatDate } from '../../../../util/dates';
 
 export async function generateMetadata(props: Props) {
   const singleAnimal = await getAnimalById(Number(props.params.animalId));
@@ -27,6 +28,7 @@ export default async function NaiveAnimalPage(props: Props) {
     <div>
       This is a single animal page
       <h1>{singleAnimal.firstName}</h1>
+      <div>Birth date: {formatDate(singleAnimal.birthDate)}</div>
       <Image
         src={`/images/${singleAnimal.firstName.toLowerCase()}.png`}
         width={200}
