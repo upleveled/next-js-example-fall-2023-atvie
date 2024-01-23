60737c37da56b60f20c8e531aa4c2d3576a090b8
Karl Horky <karl.horky@gmail.com>
2024-01-23 17:22:04 +0100

    Fix comments

--- app/(auth)/register/page.tsx
+++ app/(auth)/register/page.tsx
@@ -5,18 +5,19 @@ import RegisterForm from './RegisterForm';
 
 export default async function RegisterPage() {
   // Task: Add redirect to home if user is logged in
+
   // 1. Checking if the sessionToken cookie exists
   const sessionTokenCookie = cookies().get('sessionToken');
+
   // 2. Check if the sessionToken cookie is still valid
   const session =
     sessionTokenCookie &&
     (await getValidSessionByToken(sessionTokenCookie.value));
 
   // 3. If the sessionToken cookie is valid, redirect to home
-
   if (session) redirect('/');
-  // 4. If the sessionToken cookie is invalid or doesn't exist, show the login form
 
+  // 4. If the sessionToken cookie is invalid or doesn't exist, show the login form
   return (
     <div>
       <RegisterForm />
