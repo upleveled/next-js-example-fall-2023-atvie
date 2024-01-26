678f78faef0b5776e94328b9a47c3d6c9c83e319
Lukas Prochazka <prochazka.lu@gmail.com>
2024-01-26 11:42:26 +0100

    Refactor note styles

--- app/notes/CreateNotesForm.tsx
+++ app/notes/CreateNotesForm.tsx
@@ -1,6 +1,7 @@
 'use client';
 import { useRouter } from 'next/navigation';
 import { useState } from 'react';
+import styles from './notes.module.scss';
 
 export default function CreateNoteForm() {
   const [title, setTitle] = useState('');
@@ -9,38 +10,43 @@ export default function CreateNoteForm() {
   const router = useRouter();
 
   return (
-    <form
-      onSubmit={async (event) => {
-        event.preventDefault();
-        await fetch('/api/notes', {
-          method: 'POST',
-          body: JSON.stringify({
-            title,
-            textContent,
-          }),
-        });
-        router.refresh();
-        setTitle('');
-        setTextContent('');
-      }}
-    >
-      <label>
-        Title
-        <input
-          value={title}
-          onChange={(event) => setTitle(event.currentTarget.value)}
-        />
-      </label>
+    <div className={styles.noteForm}>
+      <div>
+        <h2>Create Note</h2>
+        <form
+          onSubmit={async (event) => {
+            event.preventDefault();
+            await fetch('/api/notes', {
+              method: 'POST',
+              body: JSON.stringify({
+                title,
+                textContent,
+              }),
+            });
+            router.refresh();
+            setTitle('');
+            setTextContent('');
+          }}
+        >
+          <label>
+            Title
+            <input
+              value={title}
+              onChange={(event) => setTitle(event.currentTarget.value)}
+            />
+          </label>
 
-      <label>
-        Note
-        <input
-          value={textContent}
-          onChange={(event) => setTextContent(event.currentTarget.value)}
-        />
-      </label>
+          <label>
+            Note
+            <input
+              value={textContent}
+              onChange={(event) => setTextContent(event.currentTarget.value)}
+            />
+          </label>
 
-      <button>Add Note</button>
-    </form>
+          <button>Add Note</button>
+        </form>
+      </div>
+    </div>
   );
 }
