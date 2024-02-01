4253384f75b21eb11d6a0e72e1a3056b589e649c
Victor Ejike Nwosu <ejikello123@yahoo.com>
2024-02-01 11:02:31 +0100

    Rearrange users database queries following RCUD

--- database/users.ts
+++ database/users.ts
@@ -6,23 +6,21 @@ export type UserWithPasswordHash = User & {
   passwordHash: string;
 };
 
-export const createUserInsecure = cache(
-  async (username: string, passwordHash: string) => {
-    const [user] = await sql<User[]>`
-      INSERT INTO
-        users (username, password_hash)
-      VALUES
-        (
-          ${username.toLowerCase()},
-          ${passwordHash}
-        )
-      RETURNING
-        id,
-        username
-    `;
-    return user;
-  },
-);
+export const getUser = cache(async (token: string) => {
+  const [user] = await sql<User[]>`
+    SELECT
+      users.id,
+      users.username
+    FROM
+      users
+      INNER JOIN sessions ON (
+        sessions.token = ${token}
+        AND sessions.user_id = users.id
+        AND sessions.expiry_timestamp > now()
+      )
+  `;
+  return user;
+});
 
 export const getUserByUsernameInsecure = cache(async (username: string) => {
   const [user] = await sql<User[]>`
@@ -51,18 +49,20 @@ export const getUserWithPasswordHashByUsernameInsecure = cache(
   },
 );
 
-export const getUser = cache(async (token: string) => {
-  const [user] = await sql<User[]>`
-    SELECT
-      users.id,
-      users.username
-    FROM
-      users
-      INNER JOIN sessions ON (
-        sessions.token = ${token}
-        AND sessions.user_id = users.id
-        AND sessions.expiry_timestamp > now()
-      )
-  `;
-  return user;
-});
+export const createUserInsecure = cache(
+  async (username: string, passwordHash: string) => {
+    const [user] = await sql<User[]>`
+      INSERT INTO
+        users (username, password_hash)
+      VALUES
+        (
+          ${username.toLowerCase()},
+          ${passwordHash}
+        )
+      RETURNING
+        id,
+        username
+    `;
+    return user;
+  },
+);
