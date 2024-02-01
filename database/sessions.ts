2cbb764689792049ef1b97a2197ff372dfd69201
Victor Ejike Nwosu <ejikello123@yahoo.com>
2024-02-01 10:35:52 +0100

    Arrange sessions queries following RCUD

--- database/sessions.ts
+++ database/sessions.ts
@@ -2,6 +2,21 @@ import { cache } from 'react';
 import { sql } from '../database/connect';
 import { Session } from '../migrations/00007-createTableSessions';
 
+export const getValidSession = cache(async (token: string) => {
+  const [session] = await sql<Pick<Session, 'id' | 'token'>[]>`
+    SELECT
+      sessions.id,
+      sessions.token
+    FROM
+      sessions
+    WHERE
+      sessions.token = ${token}
+      AND sessions.expiry_timestamp > now()
+  `;
+
+  return session;
+});
+
 export const createSession = cache(async (userId: number, token: string) => {
   const [session] = await sql<Session[]>`
     INSERT INTO
@@ -36,21 +51,6 @@ export const deleteSession = cache(async (token: string) => {
     RETURNING
       id,
       token
-  `;
-
-  return session;
-});
-
-export const getValidSession = cache(async (token: string) => {
-  const [session] = await sql<Pick<Session, 'id' | 'token'>[]>`
-    SELECT
-      sessions.id,
-      sessions.token
-    FROM
-      sessions
-    WHERE
-      sessions.token = ${token}
-      AND sessions.expiry_timestamp > now()
   `;
 
   return session;
