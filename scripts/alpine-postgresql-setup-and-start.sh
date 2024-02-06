c73bed39843f999d1fc0f9ecab209e3723a18327
Lukas Prochazka <prochazka.lu@gmail.com>
2024-02-06 13:09:27 +0100

    Remove unnecessary folder creation

--- scripts/alpine-postgresql-setup-and-start.sh
+++ scripts/alpine-postgresql-setup-and-start.sh
@@ -7,7 +7,6 @@ echo "Setting up PostgreSQL on Alpine Linux..."
 
 echo "Creating folders for PostgreSQL and adding permissions for postgres user..."
 export PGDATA=/postgres-volume/run/postgresql/data
-mkdir -p "$PGDATA"
 
 # Only allow postgres user access to data directory
 chmod 0700 "$PGDATA"
