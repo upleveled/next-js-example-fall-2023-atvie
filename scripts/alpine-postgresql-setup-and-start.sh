f7bacefa8dca20e84c38fcc80ee37458f3ba9d73
Karl Horky <karl.horky@gmail.com>
2024-02-09 18:18:24 +0100

    Add more echo commands

--- scripts/alpine-postgresql-setup-and-start.sh
+++ scripts/alpine-postgresql-setup-and-start.sh
@@ -4,27 +4,30 @@
 set -o errexit
 
 echo "Setting up PostgreSQL on Alpine Linux..."
-export PGDATA=/postgres-volume/run/postgresql/data
 
-# Only allow postgres user access to data directory
-echo "Adding permissions for postgres user..."
+export PGHOST=/postgres-volume/run/postgresql
+export PGDATA="$PGHOST/data"
+
+echo "Adding exclusive data directory permissions for postgres user..."
 chmod 0700 "$PGDATA"
+
+echo "Initializing database cluster..."
 initdb -D "$PGDATA"
 
-# Update PostgreSQL config path to use volume location if app has a volume
-sed -i "s/#unix_socket_directories = '\/run\/postgresql'/unix_socket_directories = '\/postgres-volume\/run\/postgresql'/g" /postgres-volume/run/postgresql/data/postgresql.conf
+echo "Prepending volume path to Unix Domain Socket path..."
+sed -i "s/#unix_socket_directories = '\/run\/postgresql'/unix_socket_directories = '\/postgres-volume\/run\/postgresql'/g" "$PGDATA/postgresql.conf"
 
-# Configure PostgreSQL to listen for connections from any address
-echo "listen_addresses='*'" >> $PGDATA/postgresql.conf
+echo "Enabling connections on all available IP interfaces..."
+echo "listen_addresses='*'" >> "$PGDATA/postgresql.conf"
 
 echo "Starting PostgreSQL..."
 pg_ctl start -D "$PGDATA"
 
 echo "Creating database, user and schema..."
-psql -h /postgres-volume/run/postgresql  -U postgres postgres << SQL
+psql -U postgres postgres << SQL
   CREATE DATABASE $PGDATABASE;
   CREATE USER $PGUSERNAME WITH ENCRYPTED PASSWORD '$PGPASSWORD';
   GRANT ALL PRIVILEGES ON DATABASE $PGDATABASE TO $PGUSERNAME;
-  \\connect $PGDATABASE;
+  \\connect $PGDATABASE
   CREATE SCHEMA $PGUSERNAME AUTHORIZATION $PGUSERNAME;
 SQL
