aea15ea8c800289c34b518243cb5c35f293ec0f5
Lukas Prochazka <prochazka.lu@gmail.com>
2024-02-10 11:48:54 +0100

    Add PostgreSQL script and step, remove branching (#3)
    * Update Dockerfile and add postgres script
    
    * Edit linebreaks
    
    * Run script and delete empty line
    
    * Fix PostgreSQL initialization and start scripts
    
    * Refactor fly.io scripts
    
    * Update Dockerfile and PostgreSQL scripts
    
    * Update PostgreSQL setup and start scripts
    
    * Remove unnecessary folder creation
    
    * Refactor PostgreSQL setup and permissions
    
    * Remove empty lines
    
    * Add empty lines at the end of script files
    
    * Update PostgreSQL setup and start scripts
    
    * Add more echo commands
    
    ---------
    
    Co-authored-by: Karl Horky <karl.horky@gmail.com>

--- scripts/alpine-postgresql-setup-and-start.sh
+++ scripts/alpine-postgresql-setup-and-start.sh
@@ -4,13 +4,21 @@
 set -o errexit
 
 echo "Setting up PostgreSQL on Alpine Linux..."
-export PGDATA=/var/lib/postgresql/data
-mkdir "$PGDATA"
-# Only allow postgres user access to data directory
+
+export PGHOST=/postgres-volume/run/postgresql
+export PGDATA="$PGHOST/data"
+
+echo "Adding exclusive data directory permissions for postgres user..."
 chmod 0700 "$PGDATA"
+
+echo "Initializing database cluster..."
 initdb -D "$PGDATA"
-# Log to syslog, which is rotated (older logs automatically deleted)
-sed "/^[# ]*log_destination/clog_destination = 'syslog'" -i "$PGDATA/postgresql.conf"
+
+echo "Prepending volume path to Unix Domain Socket path..."
+sed -i "s/#unix_socket_directories = '\/run\/postgresql'/unix_socket_directories = '\/postgres-volume\/run\/postgresql'/g" "$PGDATA/postgresql.conf"
+
+echo "Enabling connections on all available IP interfaces..."
+echo "listen_addresses='*'" >> "$PGDATA/postgresql.conf"
 
 echo "Starting PostgreSQL..."
 pg_ctl start -D "$PGDATA"
@@ -20,6 +28,6 @@ psql -U postgres postgres << SQL
   CREATE DATABASE $PGDATABASE;
   CREATE USER $PGUSERNAME WITH ENCRYPTED PASSWORD '$PGPASSWORD';
   GRANT ALL PRIVILEGES ON DATABASE $PGDATABASE TO $PGUSERNAME;
-  \\connect $PGDATABASE;
+  \\connect $PGDATABASE
   CREATE SCHEMA $PGUSERNAME AUTHORIZATION $PGUSERNAME;
 SQL
